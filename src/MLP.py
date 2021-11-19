5a15547c22d0c1e377bfb2652393b1119a231873
Jeremy Evert Helm <jeremy.evert@swosu.edu>
2021-11-19 06:25:30 -0600

    started to play with code.

--- src/MLP.py
+++ src/MLP.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 class Perceptron:
     """A single neuron with the sigmoid activation function.
@@ -7,8 +8,19 @@ class Perceptron:
           bias:   The bias term. By defaul it's 1.0."""
 
     def __init__(self, inputs, bias = 1.0):
-        """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
+        """Return a new Perceptron object with the specified number of inputs (+1 for the bias)."""
+        self.weights = (np.random.rand(inputs+1) * 2) - 1
+        self.bias = bias
+
+    def set_weights(self, w_init):
+        self.weights = np.array(w_init)
+
+
+    def sigmoid(self,sum):
+        return 1/(1+np.exp(-sum))
+
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        
+        sum = np.dot(np.append(x,self.bias),self.weights)
+        return self.sigmoid(sum)
